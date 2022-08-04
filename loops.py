032a786a36cb9ae521f36409e3a37af54a36b4ab
likesPuzzles <107944114+likesPuzzles@users.noreply.github.com>
2022-08-04 09:30:49 -0400

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,18 @@
+x=999
+y=999
+for x in range(999,101,-1):
+    for y in range(999,101,-1):
+        print(x,y)
+        product=x*y
+        num=str(product)
+        continue
+    # check for palindrome
+    
+        if num[::-1]==num:
+            print (product)
+            break
+    continue   
+    y =y-1
+x=x-1
+       
+        
